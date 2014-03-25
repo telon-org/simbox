5e1f060907a35a20b1c7f80ca2361b29843542ab
g4 <devnull@localhost>
2014-03-25 11:07:39 +0400

    autocommit

--- nabor/beeline_spb/commands/setdaylimit_new.sh
+++ nabor/beeline_spb/commands/setdaylimit_new.sh
@@ -13,15 +13,17 @@ group=`cat $PATH_SVISTOK_SIM_SETTINGS/$imsi.group`
 if [ "$group" -eq 5 ] ; then
 echo "$imsi $operator"
 
-echo "4000" > /var/svistok/sim/limits/$imsi.limit.1
+echo "5000" > /var/svistok/sim/limits/$imsi.limit.1
 echo "9000" > /var/svistok/sim/limits/$imsi.limit.2
 echo "9000" > /var/svistok/sim/limits/$imsi.limit.3
+fi
 
-#cp /var/svistok/sim/limits/$imsi.limit_soft.1 /var/svistok/sim/limits/$imsi.limit.1
-#cp /var/svistok/sim/limits/$imsi.limit_soft.2 /var/svistok/sim/limits/$imsi.limit.2
-#cp /var/svistok/sim/limits/$imsi.limit_soft.3 /var/svistok/sim/limits/$imsi.limit.3
-#cp /var/svistok/sim/limits/$imsi.limit_soft.4 /var/svistok/sim/limits/$imsi.limit.4
-#cp /var/svistok/sim/limits/$imsi.limit_soft.5 /var/svistok/sim/limits/$imsi.limit.5
+if [ "$group" -eq 6 ] ; then
+echo "$imsi $operator"
+
+echo "5000" > /var/svistok/sim/limits/$imsi.limit.1
+echo "9000" > /var/svistok/sim/limits/$imsi.limit.2
+echo "9000" > /var/svistok/sim/limits/$imsi.limit.3
 fi
 
 if [ "$group" -eq 11 ] ; then
