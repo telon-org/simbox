a0a4c75d90efd9d843b8e3b59a7f5ab6647195b8
g12 <devnull@localhost>
2014-03-25 11:07:01 +0400

    autocommit

---
+++ src/asterisk-11.8.1/include/asterisk/build.h
@@ -0,0 +1,11 @@
+/*
+ * build.h 
+ * Automatically generated
+ */
+#define BUILD_HOSTNAME "g12"
+#define BUILD_KERNEL "3.11.0-18-generic"
+#define BUILD_MACHINE "x86_64"
+#define BUILD_OS "Linux"
+#define BUILD_DATE "2014-03-25 06:43:59 UTC"
+#define BUILD_USER "root"
+
