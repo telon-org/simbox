1771e093bf21940fabdb5fb8700058832fd6691c
g11 <devnull@localhost>
2014-03-28 11:08:51 +0400

    autocommit

--- www/simbox/head.php
+++ www/simbox/head.php
@@ -392,7 +392,7 @@ $version=shell_exec('cd /usr/simbox && sudo hg identify');
 
 //H:i:s
 ?>
-<b><?=$hostname?></b> <?=$_SERVER['SERVER_NAME']?> <i>SimServer v.<?=$version?></i>
+<b><?=$hostname?></b> <?=$_SERVER['SERVER_NAME']?> <i>SimServer</i> <font size="-1"><?=$version?></font>
 </td>
 <td width="*" align="right">
  <?=date("d.m.y")?> <?=$uptime?>
