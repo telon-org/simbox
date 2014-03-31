9bc1963a59ba6560390d8c9e238ff961230d40b4
root <root@g14>
2014-03-31 08:02:05 +0400

    autocommit

--- src/asterisk-11.8.1/include/asterisk/build.h
+++ src/asterisk-11.8.1/include/asterisk/build.h
@@ -2,10 +2,10 @@
  * build.h 
  * Automatically generated
  */
-#define BUILD_HOSTNAME "g12"
+#define BUILD_HOSTNAME "g14"
 #define BUILD_KERNEL "3.11.0-18-generic"
 #define BUILD_MACHINE "x86_64"
 #define BUILD_OS "Linux"
-#define BUILD_DATE "2014-03-25 06:43:59 UTC"
+#define BUILD_DATE "2014-03-26 08:13:52 UTC"
 #define BUILD_USER "root"
 
