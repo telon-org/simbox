938d998024363ee7c8571e590861c08b56587eab
g4 <devnull@localhost>
2014-04-10 17:32:05 +0400

    autocommit

--- src/asterisk-11.8.1/include/asterisk/build.h
+++ src/asterisk-11.8.1/include/asterisk/build.h
@@ -2,10 +2,10 @@
  * build.h 
  * Automatically generated
  */
-#define BUILD_HOSTNAME "g14"
-#define BUILD_KERNEL "3.11.0-18-generic"
+#define BUILD_HOSTNAME "g4"
+#define BUILD_KERNEL "3.8.13.12-simbox-debug"
 #define BUILD_MACHINE "x86_64"
 #define BUILD_OS "Linux"
-#define BUILD_DATE "2014-03-26 08:13:52 UTC"
+#define BUILD_DATE "2014-04-10 11:38:03 UTC"
 #define BUILD_USER "root"
 
