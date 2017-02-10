3d95787fd2318dfe8868eadd53c2422edf633335
cpodimas@gmail.com <cpodimas@gmail.com>
2017-02-10 22:43:28 +0300

    auto

--- programmator/unlock_all.sh
+++ programmator/unlock_all.sh
@@ -1,1 +1,6 @@
-/usr/simbox/system/unlocker.sh /dev/ttyUSB0 1-7 &
+/usr/simbox/system/unlocker.sh /dev/ttyUSB2 1-7.1.1 &
+/usr/simbox/system/unlocker.sh /dev/ttyUSB8 1-7.1.2 &
+/usr/simbox/system/unlocker.sh /dev/ttyUSB5 1-7.2.3 &
+/usr/simbox/system/unlocker.sh /dev/ttyUSB14 1-7.2.4 &
+/usr/simbox/system/unlocker.sh /dev/ttyUSB11 1-7.3.3 &
+/usr/simbox/system/unlocker.sh /dev/ttyUSB17 1-7.3.4 &
